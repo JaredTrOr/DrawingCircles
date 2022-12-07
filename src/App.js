3d473941267b5e691cc0ef28dae255a67261ba9d
JaredTrOr <trujillojaredalexander@gmail.com>
2022-12-07 01:54:31 -0600

    Now it is possible to click around the webage and it can be drawn circles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [points, setPoints] = useState([]);
+
+  function handlePlaceCircle(e){
+    const {clientX, clientY} = e;
+
+    setPoints([...points, {
+      x: clientX,
+      y: clientY
+    }]);
+  }
+
   return (
-    <div className="App">
-      Hello world
+    <div className="App" onClick={handlePlaceCircle}>
+      {points.map(point => 
+        <div className='point' style={{top:point.y, left: point.x}}>
+        </div>
+      )}
     </div>
   );
 }
