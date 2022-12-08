f9a103131b2abb08ee0c2a717d241158756c14f8
JaredTrOr <trujillojaredalexander@gmail.com>
2022-12-08 17:07:52 -0600

    There was implemented the undo and redo buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 function App() {
 
   const [points, setPoints] = useState([]);
+  const [popped, setPopped] = useState([]);
 
   function handlePlaceCircle(e){
     const {clientX, clientY} = e;
@@ -14,13 +15,36 @@ function App() {
     }]);
   }
 
+  function handleUndo(){
+    const newPoints = [...points];
+    const poppedPoint = newPoints.pop();
+    if(!poppedPoint) return;
+    setPopped([...popped, poppedPoint]);
+    setPoints(newPoints);
+  }
+
+  function handleRedo(){
+    const newPopped = [...popped];
+    const poppedPoint = newPopped.pop();
+    if(!poppedPoint) return;
+    setPoints([...points, poppedPoint]);
+    setPopped(newPopped);
+  }
+
   return (
-    <div className="App" onClick={handlePlaceCircle}>
-      {points.map(point => 
-        <div className='point' style={{top:point.y, left: point.x}}>
-        </div>
-      )}
-    </div>
+    <>
+      <button className='controls' onClick={handleUndo}
+      disabled={points.length === 0}>Undo</button>
+      <button className='controls' onClick={handleRedo}
+      disabled={popped.length === 0} >Redo</button>
+
+      <div className="App" onClick={handlePlaceCircle}>
+        {points.map(point => 
+          <div className='point' style={{top:point.y, left: point.x}}>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
